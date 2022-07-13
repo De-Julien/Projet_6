b134066cddc6bb00eaf96c53201a58bb14237258
Julien <denneulin.julien@gmail.com>
2022-07-13 08:36:09 +0200

    mise en fonction du serveur

---
+++ back/server.js
@@ -0,0 +1,14 @@
+// importe le package http de node.js pour créer le serveur
+const http = require('http');
+
+// importe le fichier app.js
+const app = require('./app');
+
+// paramètrage du port avec la méthode set de Express
+app.set('port', process.env.PORT || 3000);
+
+// la méthode createServer prend en argument la fonction qui sera appelé à chaque requête reçu.
+const server = http.createServer(app);
+
+// le serveur écoute les requète sur le port
+server.listen(process.env.PORT ||3000);
